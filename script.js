4bba9dd1541a34e2c640311b91d8a63964f1dd5c
Virginia Gichira <gichiravirginia@gmail.com>
2024-03-19 12:23:13 +0300

    add

--- script.js
+++ script.js
@@ -8,22 +8,5 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-  
-    // Fetch values from the form
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-  
-    // Here you can perform further actions like sending the form data to a server or displaying it
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Message:", message);
-  
-    // Clear the form after submission
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
-  });
+
   
